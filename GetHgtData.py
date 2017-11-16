6fd875a2a29e11318b2bc4450b0076b9219742b0
shenyongqiang <saturn_syq@hotmail.com>
2017-11-16 09:54:57 +0800

    add sz market  data.

--- GetHgtData.py
+++ GetHgtData.py
@@ -51,26 +51,28 @@ def send_mail(fro, to, subject, text, files=[], mtype='plain'):
 
 # url='https://www.jisilu.cn/data/bond/?do_search=true&sort_column=&sort_order=&forall=0&treasury=1&from_rating=1&from_issuer_rating=1&from_year_left=0&from_repo=0.56&from_ytm=5.3&from_volume=0&from_market=&y1=&y2=&to_rating=99&to_issuer_rating=99&to_year_left=2&to_repo=2&to_ytm=8&to_volume='
 if __name__ == '__main__':
-    url='http://www.hkexnews.hk/sdw/search/mutualmarket_c.aspx?t=sh'
+    raw_url='http://www.hkexnews.hk/sdw/search/mutualmarket_c.aspx?t='
     try:
         conn = tamCommonLib.getMysqlConnect()
         cursor= conn.cursor()
-        result = requests.get(url)
-        soup = BeautifulSoup(result.content,"lxml")
-        area = soup.find('div',id='pnlResult')
-        fields = re.findall(r'(\d{2})/(\d{2})/(\d{4})',area.find('div').getText().strip())[0]
-        ss_date = "%s-%s-%s" % (fields[2],fields[1],fields[0])
-        data = []
-        for row in area.find_all('tr',attrs={'class':re.compile('row(0|1)')}):
-            each = []
-            for td in row.find_all('td'):
-                each.append(td.getText().strip())
-            data.append(each)
-            cursor.execute("delete from finance.hk_mainland_hist where ss_date='%s' and code='%s'" % (ss_date,each[0]))
-            vol = each[2].replace(',','')
-            percent = round(float(re.findall(r'^([0-9]*\.?[0-9]+|[0-9]+\.?[0-9]*)%$',each[3])[0])/100,4)
-            cursor.execute("insert finance.hk_mainland_hist(ss_date,code,name,volume,percent) values('%s','%s','%s',%s,%s)" % (ss_date, each[0],each[1],vol,str(percent)))
-            conn.commit()
+        for mm in ('sh','sz'):
+            url = raw_url + mm
+            result = requests.get(url)
+            soup = BeautifulSoup(result.content,"lxml")
+            area = soup.find('div',id='pnlResult')
+            fields = re.findall(r'(\d{2})/(\d{2})/(\d{4})',area.find('div').getText().strip())[0]
+            ss_date = "%s-%s-%s" % (fields[2],fields[1],fields[0])
+            data = []
+            for row in area.find_all('tr',attrs={'class':re.compile('row(0|1)')}):
+                each = []
+                for td in row.find_all('td'):
+                    each.append(td.getText().strip())
+                data.append(each)
+                cursor.execute("delete from finance.hk_mainland_hist where ss_date='%s' and code='%s'" % (ss_date,each[0]))
+                vol = each[2].replace(',','')
+                percent = round(float(re.findall(r'^([0-9]*\.?[0-9]+|[0-9]+\.?[0-9]*)%$',each[3])[0])/100,4)
+                cursor.execute("insert finance.hk_mainland_hist(ss_date,code,name,volume,percent) values('%s','%s','%s',%s,%s)" % (ss_date, each[0],each[1],vol,str(percent)))
+                conn.commit()
         #get the statistics data
         cursor.execute('drop table if exists temp.tt_ss')
         sql='''
